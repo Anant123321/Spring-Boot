5e07288687a1812707ca77cda628c4d6c7dc1184
Anant shibe <anant171087.cse@chitkara.edu.in>
2020-04-28 14:08:51 +0530

    Simple MVC Created

---
+++ src/main/java/com/sample/project1/Controller/BookController.java
@@ -0,0 +1,23 @@
+package com.sample.project1.Controller;
+
+import com.sample.project1.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/book")
+    public String getBooks(Model model){
+
+        model.addAttribute("books",bookRepository.findAll());
+        return "books";
+    }
+}
